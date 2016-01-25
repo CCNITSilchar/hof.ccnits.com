41ac42e75b6c8744a57e996f7625bd907c734401
root <root@blade6.cse.nits.ac.in>
2016-01-26 00:15:29 +0530

    removed admin connection file

--- Admin/lboard/core/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_SERVER","127.0.0.1");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","priyapandu");
-define("DB_NAME","leaderboard");
-define("PROXY","172.16.25.10:8080");
-?>
