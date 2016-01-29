667ee24334f55d6fa556acff79bb836c5a1055d3
dracula109 <akhilkgangadharan109@gmail.com>
2016-01-29 22:36:54 +0530

    Update add_contest.php

--- Admin/lboard/add_contest.php
+++ Admin/lboard/add_contest.php
@@ -1,6 +1,6 @@
 <?php
 //including configuration
-require_once 'core/config.php';
+require_once '../../config/connection.php';
 
 //including classes 
 include 'classes/coders_basic.php';
@@ -18,11 +18,12 @@ include 'functions/probability_function.php';
 //declaring contest variables 
 $contest=new contest();
 $contest_id = $_POST['contest_id'];
+$contest_name = $_POST['contest_name'];
 $contest_type = $_POST['contest_type'];
 $contest_link = $_POST['contest_link'];
 $contest_date = $_POST['contest_date'];
 //add $_POST VALUES HERE AND CONTEST WILL BE UPDATED
-$contest->add_contest($contest_id,$contest_type,$contest_date,$contest_link);
+$contest->add_contest($contest_id,$contest_name,$contest_type,$contest_date,$contest_link);
 
 //calling the function
 header('Location:../pages/updateDatabase.php');
